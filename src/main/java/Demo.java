c52e4f1a87367cce02211dfb9dd5c3a86fc177f5
weqpro <weqpro@gmail.com>
2025-10-04 23:08:59 +0300

    Demo

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,5 +1,19 @@
+import lotr.CharacterFactory;
+import lotr.GameManager;
+import lotr.Character;
+
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        CharacterFactory factory = new CharacterFactory();
+
+
+        Character c1 = factory.createCharacter();
+        Character c2 = factory.createCharacter();
+
+        System.out.println("Rolled: " + c1 + " vs " + c2);
+
+        GameManager gm = new GameManager(System.out);
+        gm.fight(c1, c2);
+
     }
 }
